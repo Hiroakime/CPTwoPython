58faf8a20401794072bcfa71e032251f8ef04be4
Hiroakime <tokotokotowa1@gmail.com>
2026-02-23 23:51:04 +0800

    Update
    Update

---
+++ Assignment/Assignment1.py
@@ -0,0 +1,17 @@
+#Madlibs Game
+PersonsName = input ("Enter a Person's Name: ")
+noun1 = input ("Enter a Noun (a place): ")
+adjective1 = input ("Enter an Adjective (describe a feeling): ")
+adjective2 = input ("Enter an another Adjective (describe a person): ")
+verb1 = input ("Enter a Verb (Enter a verb ending in -ing): ")
+adverb = input ("Enter an Adverb (describe how you do something): ")
+emotion1 = input ("Enter an Emotion (describe a feeling): ")
+adjective3 = input ("Enter an another Adjective (describe something beautiful): ")
+
+print("\nA Romantic Mad Libs story about a guy who fell in love with a girl.\n")
+
+print(f"I saw {PersonsName} for the first time at a {noun1}. I immediately felt {adjective1}.")
+print(f"She was the most {adjective2} person I had ever seen.")
+print(f"I watched her {verb1} {adverb}, and I felt a surge of {emotion1}.")
+print(f"From that moment on, I knew my life would never be the same.")
+print(f"Her smile is like a {adjective3} sunrise, and I can't wait to see her again.")
