4098db40cebea6f487beca8857c858befab19a8d
CIITSHSDTP00\CIIT <carlos09092287@gmail.com>
2026-03-09 13:11:07 +0800

    update
    update

--- Python/Section Two/CPTwoPython/Main.py
+++ Python/Section Two/CPTwoPython/Main.py
@@ -60,7 +60,7 @@ print(f"The result is: {result}")'''
 #result = text.lower()
 #print (result)
 
-Username = input("Enter your username: ")
+'''Username = input("Enter your username: ")
 
 UserInput = Username.isalpha()
 UserInput = Username.count(Username)
@@ -73,11 +73,7 @@ else:
     print("Your username is invalid. Please enter a valid username with only 12 characters or less and only alphabetic characters.")
     
 if Username.find(" ") != -1:
-    print("Your username is invalid. Please enter a valid username without spaces.")
-
-
-    
-
+    print("Your username is invalid. Please enter a valid username without spaces.")'''
 
 '''if UserInput == True:
     print("Your username is valid.")
@@ -89,3 +85,88 @@ if UserInput == 12:
     print("Your username is valid.")
 else: UserInput > 12:
     print("Your username is invalid. Please enter a valid username.")'''
+    
+'''ccnum = "1267-2456-7674-9672"
+
+ccnum1 = ccnum[15:]
+print(f"Credit card number: xxxx-xxxx-xxxx{ccnum1}")
+
+rchar = ccnum[18:-1]
+print(f"Credit card number in reverse: {rchar}")'''
+
+#EXample
+'''username = input("Enter your username: ")
+
+while username = "";
+    print ("Your username is invalid. Please enter a valid username.")
+    username = input("Enter your username: ")
+
+print(f"Your username is valid. Welcome {username}.")'''
+
+'''number = int (input("Enter a number: "))
+
+while number < 1 or number > 20:
+    if number < 1:
+        print("Your number should not be lower than 1.")
+        number = int (input("Enter a number: "))
+    elif number > 20:
+        print("Your number should not be higher than 20.")
+        number = int (input("Enter a number: "))
+    else:
+        print(f"Number is {number}.")'''
+
+'''favfood = input("What is your favorite food(enter q to quit)? ")
+
+while not favfood == "q":
+    print(f"Your favorite food is {favfood}.")
+    favfood = input("What is your favorite food(enter q to quit)? ")
+
+print("thank you for giving your favorite food!")'''
+
+'''for x in range(1, 10):
+    if x == 5:
+        break
+    else:
+        print(x)'''
+
+#Example List
+
+'''fruits = ["apple", "orange", "banana", "coconut"]
+fruits.append("pineapple")'''
+
+#list on while loop
+'''index = 0
+while index <len(fruits):
+    print(fruits[index])'''
+
+'''for x in range(len(fruits)):
+    print(f"fruit: {fruits[x]}")
+    
+for x in fruits:
+    print(f"fruit: {x}")'''
+
+#example
+
+'''def addition(num1, num2):
+    sum = num1 + num2
+    print(sum)
+    
+addition(50, 50)'''
+
+'''def info(name, age, country):
+    print(name)
+    print(age)
+    print(country)
+
+info("Bucalabels", 67, "Iran")'''
+
+class Students:
+
+    def __init__(self, name, course, gpa):
+        self.name = name
+        self.course = course
+        self.gpa = gpa
+
+student1 = Students("John", "BSIS", 3.8)
+student2 = Students("Robert", "BSCS", 3.5)
+student3 = Students("Bucala", "BSCS", 3.9)
